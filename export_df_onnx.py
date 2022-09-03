add fix broadcast_to...

export_df_onnx.py
@@ -3,23 +3,12 @@ from diffusers import StableDiffusionPipeline, LMSDiscreteScheduler
 import torch
 from transformers import CLIPTextModel
 from alfred import logger
+from torch import nn
+from stablefusion.clip_textmodel import CLIPTextModelTracable
+from stablefusion.unet_2d_condition import UNet2DConditionModelTracable
 
 
-torch_triu = torch.triu
-
-
-def triu_onnx(x, diagonal=0):
-    l = x.shape[0]
-    arange = torch.arange(l, device=x.device)
-    mask = arange.expand(l, l)
-    arange = arange.unsqueeze(-1)
-    if diagonal:
-        arange = arange + diagonal
-    mask = mask >= arange
-    return mask * x
-
-
-text_encoder = CLIPTextModel.from_pretrained(
+text_encoder = CLIPTextModelTracable.from_pretrained(
     "weights/stable-diffusion-v1-4/text_encoder", return_dict=False
 )
 
@@ -51,9 +40,6 @@ def convert_to_onnx(
     p = Path("weights/onnx/")
     p.mkdir(parents=True, exist_ok=True)
 
-    # override default trilu
-    torch.triu = triu_onnx
-
     if height % 8 != 0 or width % 8 != 0:
         raise ValueError(
             f"`height` and `width` have to be divisible by 8 but are {height} and {width}."
@@ -72,11 +58,12 @@ def convert_to_onnx(
             torch.rand(2, 12, 768),
         ),  # batch change, text embed with no trunc
     ]
-    traced_model = torch.jit.trace(
-        unet, check_inputs[0], check_inputs=[check_inputs[1]], strict=True
-    )
+    # traced_model = torch.jit.trace(
+    #     unet, check_inputs[0], check_inputs=[check_inputs[1]], strict=True
+    # )
     torch.onnx.export(
-        traced_model,
+        # traced_model,
+        unet,
         check_inputs[0],
         p / "unet.onnx",
         input_names=["latent_model_input", "t", "encoder_hidden_states"],
@@ -85,7 +72,7 @@ def convert_to_onnx(
             "t": [0],
             "encoder_hidden_states": [0, 1],
         },
-        opset_version=16,
+        opset_version=12,
     )
     logger.info("unet saved.")
 
@@ -100,7 +87,7 @@ def convert_to_onnx(
         p / "post_quant_conv.onnx",
         input_names=["latents"],
         dynamic_axes={"latents": [0]},
-        opset_version=14,
+        opset_version=12,
     )
 
     # decoder onnx export
@@ -114,7 +101,7 @@ def convert_to_onnx(
         p / "decoder.onnx",
         input_names=["latents"],
         dynamic_axes={"latents": [0]},
-        opset_version=14,
+        opset_version=12,
     )
     logger.info("vae decoder saved.")
 
@@ -123,16 +110,21 @@ def convert_to_onnx(
         (torch.randint(1, 24000, (1, 77)),),
         (torch.randint(1, 24000, (2, 77)),),
     ]
-    traced_model = torch.jit.trace(
-        text_encoder, check_inputs[0], check_inputs=[check_inputs[1]], strict=False
-    )
+
+    # torch.triu = triu_onnx
+    # traced_model = torch.jit.trace(
+    #     text_encoder, check_inputs[0], check_inputs=[check_inputs[1]], strict=False
+    # )
+
+    # text_encoder = replace_module(text_encoder, nn.Triu, triu_onnx)
     torch.onnx.export(
-        traced_model,
+        # traced_model,
+        text_encoder,
         check_inputs[0],
         p / "encoder.onnx",
         input_names=["text_input"],
         dynamic_axes={"text_input": [0, 1]},
-        opset_version=14,
+        opset_version=12,
     )
     logger.info("vae encoder saved.")
 

stablefusion/stablefusion_ov_engine.py
@@ -1,17 +1,22 @@
-'''
+"""
 we don't need saftychecker!!
-'''
+"""
 import inspect
 import numpy as np
+
 # openvino
 from openvino.runtime import Core
+
 # tokenizer
 from transformers import CLIPTokenizer
+
 # utils
 from tqdm import tqdm
 from huggingface_hub import hf_hub_download
 from diffusers import LMSDiscreteScheduler, PNDMScheduler
 import cv2
+import os
+from alfred import logger
 
 
 def result(var):
@@ -20,40 +25,69 @@ def result(var):
 
 class StableDiffusionEngine:
     def __init__(
-            self,
-            scheduler,
-            model="bes-dev/stable-diffusion-v1-4-openvino",
-            tokenizer="openai/clip-vit-large-patch14",
-            device="CPU"
+        self,
+        scheduler,
+        model="bes-dev/stable-diffusion-v1-4-openvino",
+        local_model_path=None,
+        tokenizer="openai/clip-vit-large-patch14",
+        int8=False,
+        device="CPU",
     ):
         self.tokenizer = CLIPTokenizer.from_pretrained(tokenizer)
         self.scheduler = scheduler
         # models
         self.core = Core()
+
+        load_local = False
+        if os.path.exists(local_model_path):
+            logger.info(
+                f"detected model in local path, loading onnx model: {local_model_path}"
+            )
+            load_local = True
+
         # text features
-        self._text_encoder = self.core.read_model(
-            hf_hub_download(repo_id=model, filename="text_encoder.xml"),
-            hf_hub_download(repo_id=model, filename="text_encoder.bin")
-        )
+        if load_local:
+            self._text_encoder = self.core.read_model(
+                os.path.join(local_model_path, "encoder.onnx")
+            )
+            logger.info("text encoder read.")
+        else:
+            self._text_encoder = self.core.read_model(
+                hf_hub_download(repo_id=model, filename="text_encoder.xml"),
+                hf_hub_download(repo_id=model, filename="text_encoder.bin"),
+            )
         self.text_encoder = self.core.compile_model(self._text_encoder, device)
+
         # diffusion
         self._unet = self.core.read_model(
             hf_hub_download(repo_id=model, filename="unet.xml"),
-            hf_hub_download(repo_id=model, filename="unet.bin")
+            hf_hub_download(repo_id=model, filename="unet.bin"),
         )
         self.unet = self.core.compile_model(self._unet, device)
         self.latent_shape = tuple(self._unet.inputs[0].shape)[1:]
+
         # decoder
-        self._vae_decoder = self.core.read_model(
-            hf_hub_download(repo_id=model, filename="vae_decoder.xml"),
-            hf_hub_download(repo_id=model, filename="vae_decoder.bin")
-        )
+        if load_local:
+            self._vae_decoder = self.core.read_model(
+                os.path.join(local_model_path, "decoder.onnx")
+            )
+        else:
+            self._vae_decoder = self.core.read_model(
+                hf_hub_download(repo_id=model, filename="vae_decoder.xml"),
+                hf_hub_download(repo_id=model, filename="vae_decoder.bin"),
+            )
         self.vae_decoder = self.core.compile_model(self._vae_decoder, device)
+
         # encoder
-        self._vae_encoder = self.core.read_model(
-            hf_hub_download(repo_id=model, filename="vae_encoder.xml"),
-            hf_hub_download(repo_id=model, filename="vae_encoder.bin")
-        )
+        if load_local:
+            self._vae_encoder = self.core.read_model(
+                os.path.join(local_model_path, "post_quant_conv.onnx")
+            )
+        else:
+            self._vae_encoder = self.core.read_model(
+                hf_hub_download(repo_id=model, filename="vae_encoder.xml"),
+                hf_hub_download(repo_id=model, filename="vae_encoder.bin"),
+            )
         self.vae_encoder = self.core.compile_model(self._vae_encoder, device)
         self.init_image_shape = tuple(self._vae_encoder.inputs[0].shape)[2:]
 
@@ -63,12 +97,12 @@ class StableDiffusionEngine:
             mask = cv2.resize(
                 mask,
                 (self.init_image_shape[1], self.init_image_shape[0]),
-                interpolation = cv2.INTER_NEAREST
+                interpolation=cv2.INTER_NEAREST,
             )
         mask = cv2.resize(
             mask,
             (self.init_image_shape[1] // 8, self.init_image_shape[0] // 8),
-            interpolation = cv2.INTER_NEAREST
+            interpolation=cv2.INTER_NEAREST,
         )
         mask = mask.astype(np.float32) / 255.0
         mask = np.tile(mask, (4, 1, 1))
@@ -83,7 +117,7 @@ class StableDiffusionEngine:
             image = cv2.resize(
                 image,
                 (self.init_image_shape[1], self.init_image_shape[0]),
-                interpolation=cv2.INTER_LANCZOS4
+                interpolation=cv2.INTER_LANCZOS4,
             )
         # normalize
         image = image.astype(np.float32) / 255.0
@@ -93,30 +127,32 @@ class StableDiffusionEngine:
         return image
 
     def _encode_image(self, init_image):
-        moments = result(self.vae_encoder.infer_new_request({
-            "init_image": self._preprocess_image(init_image)
-        }))
+        moments = result(
+            self.vae_encoder.infer_new_request(
+                {"init_image": self._preprocess_image(init_image)}
+            )
+        )
         mean, logvar = np.split(moments, 2, axis=1)
         std = np.exp(logvar * 0.5)
         latent = (mean + std * np.random.randn(*mean.shape)) * 0.18215
         return latent
 
     def __call__(
-            self,
-            prompt,
-            init_image = None,
-            mask = None,
-            strength = 0.5,
-            num_inference_steps = 32,
-            guidance_scale = 7.5,
-            eta = 0.0
+        self,
+        prompt,
+        init_image=None,
+        mask=None,
+        strength=0.5,
+        num_inference_steps=32,
+        guidance_scale=7.5,
+        eta=0.0,
     ):
         # extract condition
         tokens = self.tokenizer(
             prompt,
             padding="max_length",
             max_length=self.tokenizer.model_max_length,
-            truncation=True
+            truncation=True,
         ).input_ids
         text_embeddings = result(
             self.text_encoder.infer_new_request({"tokens": np.array([tokens])})
@@ -128,15 +164,21 @@ class StableDiffusionEngine:
                 "",
                 padding="max_length",
                 max_length=self.tokenizer.model_max_length,
-                truncation=True
+                truncation=True,
             ).input_ids
             uncond_embeddings = result(
-                self.text_encoder.infer_new_request({"tokens": np.array([tokens_uncond])})
+                self.text_encoder.infer_new_request(
+                    {"tokens": np.array([tokens_uncond])}
+                )
             )
-            text_embeddings = np.concatenate((uncond_embeddings, text_embeddings), axis=0)
+            text_embeddings = np.concatenate(
+                (uncond_embeddings, text_embeddings), axis=0
+            )
 
         # set timesteps
-        accepts_offset = "offset" in set(inspect.signature(self.scheduler.set_timesteps).parameters.keys())
+        accepts_offset = "offset" in set(
+            inspect.signature(self.scheduler.set_timesteps).parameters.keys()
+        )
         extra_set_kwargs = {}
         offset = 0
         if accepts_offset:
@@ -153,7 +195,9 @@ class StableDiffusionEngine:
             init_latents = self._encode_image(init_image)
             init_timestep = int(num_inference_steps * strength) + offset
             init_timestep = min(init_timestep, num_inference_steps)
-            timesteps = np.array([[self.scheduler.timesteps[-init_timestep]]]).astype(np.long)
+            timesteps = np.array([[self.scheduler.timesteps[-init_timestep]]]).astype(
+                np.long
+            )
             noise = np.random.randn(*self.latent_shape)
             latents = self.scheduler.add_noise(init_latents, noise, timesteps)[0]
 
@@ -170,7 +214,9 @@ class StableDiffusionEngine:
         # eta (η) is only used with the DDIMScheduler, it will be ignored for other schedulers.
         # eta corresponds to η in DDIM paper: https://arxiv.org/abs/2010.02502
         # and should be between [0, 1]
-        accepts_eta = "eta" in set(inspect.signature(self.scheduler.step).parameters.keys())
+        accepts_eta = "eta" in set(
+            inspect.signature(self.scheduler.step).parameters.keys()
+        )
         extra_step_kwargs = {}
         if accepts_eta:
             extra_step_kwargs["eta"] = eta
@@ -178,36 +224,50 @@ class StableDiffusionEngine:
         t_start = max(num_inference_steps - init_timestep + offset, 0)
         for i, t in tqdm(enumerate(self.scheduler.timesteps[t_start:])):
             # expand the latents if we are doing classifier free guidance
-            latent_model_input = np.stack([latents, latents], 0) if guidance_scale > 1.0 else latents[None]
+            latent_model_input = (
+                np.stack([latents, latents], 0)
+                if guidance_scale > 1.0
+                else latents[None]
+            )
             if isinstance(self.scheduler, LMSDiscreteScheduler):
                 sigma = self.scheduler.sigmas[i]
-                latent_model_input = latent_model_input / ((sigma**2 + 1) ** 0.5)
+                latent_model_input = latent_model_input / ((sigma ** 2 + 1) ** 0.5)
 
             # predict the noise residual
-            noise_pred = result(self.unet.infer_new_request({
-                "latent_model_input": latent_model_input,
-                "t": t,
-                "encoder_hidden_states": text_embeddings
-            }))
+            noise_pred = result(
+                self.unet.infer_new_request(
+                    {
+                        "latent_model_input": latent_model_input,
+                        "t": t,
+                        "encoder_hidden_states": text_embeddings,
+                    }
+                )
+            )
 
             # perform guidance
             if guidance_scale > 1.0:
-                noise_pred = noise_pred[0] + guidance_scale * (noise_pred[1] - noise_pred[0])
+                noise_pred = noise_pred[0] + guidance_scale * (
+                    noise_pred[1] - noise_pred[0]
+                )
 
             # compute the previous noisy sample x_t -> x_t-1
             if isinstance(self.scheduler, LMSDiscreteScheduler):
-                latents = self.scheduler.step(noise_pred, i, latents, **extra_step_kwargs)["prev_sample"]
+                latents = self.scheduler.step(
+                    noise_pred, i, latents, **extra_step_kwargs
+                )["prev_sample"]
             else:
-                latents = self.scheduler.step(noise_pred, t, latents, **extra_step_kwargs)["prev_sample"]
+                latents = self.scheduler.step(
+                    noise_pred, t, latents, **extra_step_kwargs
+                )["prev_sample"]
 
             # masking for inapinting
             if mask is not None:
                 init_latents_proper = self.scheduler.add_noise(init_latents, noise, t)
                 latents = ((init_latents_proper * mask) + (latents * (1 - mask)))[0]
 
-        image = result(self.vae_decoder.infer_new_request({
-            "latents": np.expand_dims(latents, 0)
-        }))
+        image = result(
+            self.vae_decoder.infer_new_request({"latents": np.expand_dims(latents, 0)})
+        )
 
         # convert tensor to opencv's image format
         image = (image / 2 + 0.5).clip(0, 1)