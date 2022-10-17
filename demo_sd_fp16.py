bf7f9eeccbfc42fb83d3c2e3707b5eca550b61d7
Your Name <you@example.com>
2022-10-17 19:45:49 +0800

    add

---
+++ demo_sd_fp16.py
@@ -0,0 +1,18 @@
+from stablefusion.stablefusion_ov_engine import StableDiffusionEngine
+from diffusers.pipelines import StableDiffusionPipeline
+import torch
+import cv2
+
+
+pipe = StableDiffusionPipeline.from_pretrained(
+    "weights/stable-diffusion-v1-4",
+    revision="fp16",
+    torch_dtype=torch.float16,
+    safety_checker=None,
+)
+pipe = pipe.to("cuda")
+
+prompt = "a photo of an astronaut riding a horse on mars"
+image = pipe(prompt).images[0]
+image.save("res.png")
+# cv2.imwrite(f"res.png", image.cpu().numpy())
